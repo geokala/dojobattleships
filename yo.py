a979c685e22a663492c098264d2abfc49eaab9ab
Madda <nobody.youknow@local>
2015-07-02 21:15:57 +0100

    Board is yo friend

--- yo.py
+++ yo.py
@@ -6,6 +6,25 @@ x = 'ABCDEFGHIJ'
 y = range(1, 11)
 
 
+def showboard(hits, adjacents, moves):
+    print('    {}'.format(' '.join(x)))
+    for ypos in range(len(y)):
+        print('{:3} '.format(y[ypos]), end='')
+        for xpos in range(len(x)):
+            p = (xpos, ypos)
+            if p in hits:
+                print('x', end='')
+            elif p in adjacents:
+                print('?', end='')
+            elif p in moves:
+                print('.', end='')
+            else:
+                print('o', end='')
+            print(' ', end='')
+        print()
+    print()
+
+
 def showmove(move):
     return '{}{}'.format(x[move[0]], y[move[1]])
 
@@ -34,6 +53,7 @@ while moves:
     # Caveat: If the other player wins first, it won't win... I'm not sure this is helpful.
     adjacents = list(set(itertools.chain.from_iterable(find_adjacents(hit) for hit in hits)) &
         set(moves))
+    showboard(hits, adjacents, moves)
     if adjacents:
         move = random.choice(adjacents)
         moves.remove(move)
@@ -42,7 +62,7 @@ while moves:
     print(showmove(move))
     while True:
         result = input().lower().strip()
-        if result in 'mhs':
+        if result and result in 'mhs':
             break
     if result == 'h':
         hits.add(move)
